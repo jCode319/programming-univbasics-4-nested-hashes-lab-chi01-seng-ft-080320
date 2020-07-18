aa859161f4ece58fc6a4a0bfc82194c402940d69
Jarrell Jackson <jdjack3@gmail.com>
2020-07-18 13:53:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -140,7 +140,7 @@ def adding_to_dennis
     }
 
 def adding_to_dennis
-	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash[:dennis_ritchie][:languages].push ("Assembly")
 	puts programmer_hash
 
 end
